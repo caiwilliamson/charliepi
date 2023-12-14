794a0652cc1a953a19e03c3ace9f42b277b055a8
Cai Williamson <caiwilliamson@users.noreply.github.com>
2023-12-14 23:13:29 +0000

    Fix SHT30 timeout bug by removing sleep call

--- app/sht_30.py
+++ app/sht_30.py
@@ -1,11 +1,9 @@
 import smbus
 import time
 
-bus = smbus.SMBus(1)
-
 def read_sht_30():
+    bus = smbus.SMBus(1)
     bus.write_i2c_block_data(0x44, 0x2C, [0x06])
-    time.sleep(0.5)
     data = bus.read_i2c_block_data(0x44, 0x00, 6)
     cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45
     humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
