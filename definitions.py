860305df8beabad932c805c36f98a0bbb8d64bf3
Cai Williamson <caiwilliamson@users.noreply.github.com>
2023-12-18 18:15:03 +0000

    Add setup_logging module

--- definitions.py
+++ definitions.py
@@ -1,3 +1,4 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR_NAME = os.path.basename(ROOT_DIR)
---
+++ src/lib/setup_logging.py
@@ -0,0 +1,19 @@
+import os
+import logging
+from definitions import ROOT_DIR_NAME
+
+def setup_logging(file_name):
+    home_directory = os.path.expanduser("~")
+    log_directory = f"{home_directory}/log/{ROOT_DIR_NAME}"
+    log_file = os.path.join(log_directory, f"{file_name}.log")
+
+    os.makedirs(log_directory, exist_ok=True)
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - [%(levelname)s] - %(message)s",
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler(log_file)
+        ]
+    )
